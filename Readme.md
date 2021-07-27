e08ceacd1ce8d50d99c840ada2b6c2166dcf25a9
Dorokhovich Mikhail <mikhail@dorokhovich.ru>
2021-07-28 00:37:19 +0300

    added few documentation

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# Wishlist SSR
+
+## Structure
+1. src - source folder
+2. dist - destination folder
+3. index.js - entry point, main file
+
+For SSR uses PUG library
+
+## How to start
+to run application you have to install all dependencies with `npm i` and then just `npm start`
--- index.js
+++ index.js
@@ -2,11 +2,20 @@ const pug = require('pug');
 const fs = require('fs');
 const path = require('path');
 
-const PAGES_FOLDER = path.resolve('src', 'pages'); // `${__dirname}/src/pages`
-const DIST_FOLDER = path.resolve('dist'); // `${__dirname}/dist`
+// У нас есть папка в которую мы складываем генерируемые HTML файлы
+// А также есть папка в которой лежат все наши исходные файлы, например pages/product.pug
+// https://nodejs.org/api/path.html#path_path_resolve_paths
+// path.resolve - возвращает абсолютный путь к файлу или папке
+const PAGES_FOLDER = path.resolve('src', 'pages');
+const DIST_FOLDER = path.resolve('dist');
+// Данный флаг лучше вынести в переменную
 const PRETTY_FLAG = true;
 
+// Создаем папку для генируемых файлов HTML
+// Если папка существует  - ничего не делаем
+// https://nodejs.org/api/fs.html#fs_fs_existssync_path
 if (!fs.existsSync(DIST_FOLDER)) {
+    // https://nodejs.org/api/fs.html#fs_fs_mkdirsync_path_options
     fs.mkdirSync(DIST_FOLDER);
 }
 
@@ -19,16 +28,27 @@ const errorHandler = (filename, filePath) => (err) => {
     }
 };
 
+// Выбираем все файлы из попки куда надо складывать HTML файлы
+// https://nodejs.org/api/fs.html#fs_fs_readdirsync_path_options
 const files = fs.readdirSync(PAGES_FOLDER);
-console.log(files);
 
-files.forEach((filename) => {
-    const chunks = filename.split('.');
-    const filePath = path.join(PAGES_FOLDER, filename);
+files.forEach((originalFilename) => {
+    // Удаляем расширение файла из имени файла
+    // Разбиваем название файла по точкам
+    // Например product.template.pug => ['product', 'template', 'pug']
+    const chunks = originalFilename.split('.');
+    // Соединяем название файла точками, за исключением последнего элемента ([...].join(...))
+    // ['product', 'template', 'pug'] => product.template.html
+    const targetFilename = `${chunks.slice(0, chunks.length - 1).join('.')}.html`;
+    // Сгеренируем пути до original и target файлов
+    // https://nodejs.org/api/path.html#path_path_join_paths
+    // path.join - просто соединяем элементы массива в путь к файлв или папке
+    const originalFilePath = path.join(PAGES_FOLDER, originalFilename);
+    const targetFilePath = path.join(DIST_FOLDER, targetFilename);
     fs.writeFile(
-        path.join(DIST_FOLDER, `${chunks.slice(0, chunks.length - 1).join('.')}.html`),
-        pug.renderFile(filePath, { pretty: PRETTY_FLAG }),
-        errorHandler(filename, filePath)
+        targetFilePath,
+        pug.renderFile(originalFilePath, { pretty: PRETTY_FLAG }),
+        errorHandler(originalFilename, targetFilePath)
     );
 });
 
